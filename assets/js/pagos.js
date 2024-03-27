3a831f620c02696f9af449427b092ff124e44e04
Zannian Mantilla <zanniancelis@gmail.com>
2024-03-27 12:01:31 -0500

    pagos1

---
+++ assets/js/pagos.js
@@ -0,0 +1,22 @@
+<form>
+<script
+  src="https://checkout.wompi.co/widget.js"
+  data-render="button"
+  data-public-key="pub_test_oPWG4VBZ0tUZNwkyOouQCrZRsUSIG4ej"
+  data-currency="COP"
+  data-amount-in-cents="10000000"
+  data-reference="987XYZ"
+  data-signature:integrity="test_integrity_pzIaCf67JcS9u2eEE4Dx4ya0yt2MuaDU"
+  data-redirect-url="https://transaction-redirect.wompi.co/check"
+  data-tax-in-cents:vat="1290000"
+  data-customer-data:legal-id="123456789"
+  data-customer-data:legal-id-type="CC"
+  data-shipping-address:address-line-1="Carrera 123 # 4-5"
+  data-shipping-address:address-line-2="apto 123"
+  data-shipping-address:country="CO"
+  data-shipping-address:city="Bogota"
+  data-shipping-address:phone-number="3019988888"
+  data-shipping-address:region="Cundinamarca"
+  data-shipping-address:name="Pedro Perez"
+></script>
+</form> 
